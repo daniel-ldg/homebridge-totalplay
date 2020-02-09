3695209f1c2e5f3c10073c538a259e62ec2c06a1
daniel-ldg <daniel.ldg@icloud.com>
2020-02-09 03:12:29 -0600

    Add service
    Tv speaker service

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class TotalplayAccessory {
         this.name = config.name;
         this.ipAddress = config.ipAddress;
         this.inputs = config.inputs || [];
-    };
+    }
 
     getServices() {
         let services = [];
@@ -39,26 +39,37 @@ class TotalplayAccessory {
             .on("set", this.setActive.bind(this));
 
         tvService.getCharacteristic(Characteristic.ActiveIdentifier)
-            .on('set', (inputIdentifier, callback) => {
+            .on("set", (inputIdentifier, callback) => {
                 this.setInput(callback, this.inputReferences[inputIdentifier]);
             })
-            .on('get', this.getInput.bind(this));
+            .on("get", this.getInput.bind(this));
 
         tvService.setCharacteristic(Characteristic.ConfiguredName, this.name);
-        tvService.setCharacteristic(Characteristic.SleepDiscoveryMode, Characteristic.SleepDiscoveryMode.NOT_DISCOVERABLE);
+        tvService.setCharacteristic(Characteristic.SleepDiscoveryMode, Characteristic.SleepDiscoveryMode.ALWAYS_DISCOVERABLE);
+
+        // Configure HomeKit TV Volume Control
+        let tvSpeakerService = new Service.TelevisionSpeaker(this.name, 'TelevisionSpeaker');
+        tvSpeakerService.setCharacteristic(Characteristic.Active, Characteristic.Active.ACTIVE)
+            .setCharacteristic(Characteristic.VolumeControlType, Characteristic.VolumeControlType.RELATIVE);
+
+        tvSpeakerService.getCharacteristic(Characteristic.VolumeSelector)
+            .on("set", this.setVolume.bind(this));
+
+        tvSpeakerService.getCharacteristic(Characteristic.Mute)
+            .on('get', this.getMute.bind(this))
+            .on('set', this.setMute.bind(this));
 
 
-
-        services.push(tvService);
+        services.push(tvService, tvSpeakerService);
         return services;
-    };
+    }
 
     async sendCommand(command) {
         await rp("http://" + this.ipAddress + "/RemoteControl/KeyHandling/sendKey?key=" + command)
             .catch(function (reason) {
                 //ignore
             });
-    };
+    }
 
     async sendCommands(commands) {
         for (const command of commands) {
@@ -67,25 +78,50 @@ class TotalplayAccessory {
                 setTimeout(resolve, 200)
             })
         }
-    };
+    }
 
     getActive(callback) {
         if (callback) {
             callback(null, true);
         }
-    };
+    }
 
     setActive(state, callback) {
         this.sendCommand("on_off");
-        callback(null, !state);
-    };
+        callback(null, state);
+    }
+
+    setVolume(value, callback) {
+        let command = "";
+        switch (value) {
+            case Characteristic.VolumeSelector.DECREMENT:
+                command = "volume_down";
+                break;
+            case Characteristic.VolumeSelector.INCREMENT:
+                command = "volume_up";
+                break;
+        }
+        if (command !== "") {
+            this.sendCommand(command);
+        }
+        callback(null, value);
+    }
+
+    getMute(callback) {
+        callback(null, false)
+    }
+
+    setMute(state, callback) {
+        this.sendCommand("mute");
+        callback(null, state);
+    }
 
     setInput() {
 
-    };
+    }
 
     getInput() {
 
-    };
+    }
 
 }
